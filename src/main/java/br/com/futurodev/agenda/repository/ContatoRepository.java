04f909c677c82c140bd881b78f149966406de11c
Laura Bristot <107277969+laurabristot@users.noreply.github.com>
2022-10-17 23:09:05 -0300

    Update ContatoRepository.java

--- src/main/java/br/com/futurodev/agenda/repository/ContatoRepository.java
+++ src/main/java/br/com/futurodev/agenda/repository/ContatoRepository.java
@@ -3,7 +3,10 @@ package br.com.futurodev.agenda.repository;
 import br.com.futurodev.agenda.model.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
 
 @Repository
 public interface ContatoRepository extends JpaRepository<Contato, Long> {
+  @Query(value = "select c from Contato c where c.nome like %?1%")
+    ArrayList<Contato> getContatoByName(String nome);
 }
